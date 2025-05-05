3714f1f5d5e25b89af588897d549ec5edc61dd1e
Ilmar7786 <ilmar7786@yandex.ru>
2025-05-05 21:58:27 +0300

    style: format code according to linting rules

--- src/WebSocketClient.ts
+++ src/WebSocketClient.ts
@@ -1,70 +1,63 @@
-import { type WebSocket as NodeWS } from "ws";
+import NodeWebSocket, { type WebSocket as NodeWS } from 'ws'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AnyType = any
 
 export type WebSocketEventMap = {
-  open: Event;
-  message: MessageEvent;
-  close: CloseEvent;
-  error: Event;
-};
+  open: Event
+  message: MessageEvent
+  close: CloseEvent
+  error: Event
+}
 
 export abstract class BaseWebSocketClient {
-  protected socket: WebSocket | NodeWS;
-  protected url: string;
-  protected protocols?: string | string[];
+  protected socket: WebSocket | NodeWS
+  protected url: string
+  protected protocols?: string | string[]
 
   constructor(url: string, protocols?: string | string[]) {
-    this.url = url;
-    this.protocols = protocols;
-    this.socket = this.createWebSocket();
+    this.url = url
+    this.protocols = protocols
+    this.socket = this.createWebSocket()
   }
 
-  protected abstract createWebSocket(): WebSocket | NodeWS;
+  protected abstract createWebSocket(): WebSocket | NodeWS
 
-  on<K extends keyof WebSocketEventMap>(
-    event: K,
-    listener: (event: WebSocketEventMap[K]) => void
-  ): void {
-    (this.socket as any).addEventListener(event, listener);
+  on<K extends keyof WebSocketEventMap>(event: K, listener: (event: WebSocketEventMap[K]) => void): void {
+    this.socket.addEventListener(event, listener as AnyType)
   }
 
   send(data: string | ArrayBuffer | Blob | ArrayBufferView): void {
-    this.socket.send(data);
+    this.socket.send(data)
   }
 
   close(code?: number, reason?: string): void {
-    this.socket.close(code, reason);
+    this.socket.close(code, reason)
   }
 
   get readyState(): number {
-    return this.socket.readyState;
+    return this.socket.readyState
   }
 }
 
 class BrowserWebSocketClient extends BaseWebSocketClient {
   protected createWebSocket(): WebSocket {
-    return new WebSocket(this.url, this.protocols);
+    return new WebSocket(this.url, this.protocols)
   }
 }
 
 class NodeWebSocketClient extends BaseWebSocketClient {
   protected createWebSocket(): NodeWS {
-    const WebSocket = require("ws");
-    return new WebSocket(this.url, this.protocols);
+    return new NodeWebSocket(this.url, this.protocols)
   }
 }
 
 export class WebSocketClient {
-  static create(
-    url: string,
-    protocols?: string | string[]
-  ): BaseWebSocketClient {
-    if (
-      typeof window !== "undefined" &&
-      typeof window.WebSocket !== "undefined"
-    ) {
-      return new BrowserWebSocketClient(url, protocols);
+  static create(url: string, protocols?: string | string[]): BaseWebSocketClient {
+    if (typeof window !== 'undefined' && typeof window.WebSocket !== 'undefined') {
+      return new BrowserWebSocketClient(url, protocols)
     } else {
-      return new NodeWebSocketClient(url, protocols);
+      return new NodeWebSocketClient(url, protocols)
     }
   }
 }
--- src/utils/configurationUrlWs.ts
+++ src/utils/configurationUrlWs.ts
@@ -1,23 +1,18 @@
 interface Options {
-  basePath: string;
-  endpoint: string;
-  token: string;
-  interval: string | number;
+  basePath: string
+  endpoint: string
+  token: string
+  interval: string | number
 }
 
-export const configurationUrlWs = ({
-  basePath,
-  endpoint,
-  interval,
-  token,
-}: Options) => {
-  const url = new URL(basePath);
-  const wsProtocol = url.protocol === "https:" ? "wss" : "ws";
+export const configurationUrlWs = ({ basePath, endpoint, interval, token }: Options) => {
+  const url = new URL(basePath)
+  const wsProtocol = url.protocol === 'https:' ? 'wss' : 'ws'
 
   url.protocol = wsProtocol
   url.pathname = endpoint
   url.searchParams.set('interval', interval.toString())
   url.searchParams.set('token', token)
 
-  return url.toString();
-};
+  return url.toString()
+}
